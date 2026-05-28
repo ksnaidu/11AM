2151f1b62f89a8aa46c373671a6a94a3ee308730
ksnaidu <ksnaidudba@gmail.com>
2026-05-28 12:20:33 +0530

    ec2 created

---
+++ terraform/variables/variables.tf
@@ -0,0 +1,44 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+    description = "Ami id of RHEL9"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type = map(string)
+    default = {
+      name = "Hello-world"
+      purpose = "variables-demo"
+
+    }  
+}
+
+variable "sg_name" {
+    default = "allow-all"
+  
+}
+
+variable "from_port" {
+    default = 0  
+}
+
+variable "to_port" {
+    type = number
+    default = 0
+  
+}
+
+variable "cidr_blocks" {
+     type = list(string)
+     default = [ "0.0.0.0/0" ] 
+}
+
+variable "sg_tags" {
+     default = {
+        Name = "allow-all"
+     }  
+}
