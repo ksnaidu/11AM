dc706df67f7638869facdfb27bb1983f8ec32e60
ksnaidu <ksnaidudba@gmail.com>
2026-06-10 18:57:41 +0530

    Convert tfvars from gitlink

---
+++ terraform-multi-env-1/tfvars/dev/backend.tf
@@ -0,0 +1,5 @@
+bucket = "11am-remote-state-dev"
+key    = "tfvars-dev-demo"
+region = "us-east-1"
+encrypt        = true
+use_lockfile = true 
---
+++ terraform-multi-env-1/tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket = "11am-remote-state-prod"
+key    = "tfvars-prod-demo"
+region = "us-east-1"
+encrypt        = true
+use_lockfile = true 
