825c61145b28ac22f07db990f46feaeadff7db98
ksnaidu <ksnaidudba@gmail.com>
2026-07-01 11:24:34 +0530

    roboshop-infra-dev

---
+++ roboshop-infra-dev/main.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+    source = "git::https://github.com/ksnaidu/11AM.git//terraform-aws-vpc?ref=main"
+   
+    project = var.project
+    environment = var.environment
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+    is_peering_required = true
+
+}
+
