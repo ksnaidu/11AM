d2bacc4d37f2d9ad01020389e72a560b41df213c
ksnaidu <ksnaidudba@gmail.com>
2026-07-02 20:05:13 +0530

    parameters added

--- roboshop-infra-dev/10-sg/main.tf
+++ roboshop-infra-dev/10-sg/main.tf
@@ -5,7 +5,10 @@
 
     sg_name = var.frontend_sg_name
     sg_description = var.frontend_sg_description
-    vpc_id = local.vpc_id  ##vpc-id stored in ssm parameter after create.
+    vpc_id = local.vpc_id ##vpc-id stored in ssm parameter after create.
 }
+
+
   
+
 
