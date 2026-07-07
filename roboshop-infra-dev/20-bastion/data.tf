4016e05ffcdb4ea44f4361b945851bac7664d097
ksnaidu <ksnaidudba@gmail.com>
2026-07-07 12:26:18 +0530

    terraform

--- roboshop-infra-dev/20-bastion/data.tf
+++ roboshop-infra-dev/20-bastion/data.tf
@@ -1,27 +1,24 @@
 data "aws_ami" "joindevops" {
-  owners           = ["196939353460"]
-  most_recent      = true
+  owners      = ["973714476881"]
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice*"]
+  }
 
-filter {
-    name= "name"
-    values = ["RHEL-9-Devops-Practice"]
-}
-
-filter {
-    name = "root-device-type"
+  filter {
+    name   = "root-device-type"
     values = ["ebs"]
-}
+  }
 
-filter {
-    name = "virtualization-type"
+  filter {
+    name   = "virtualization-type"
     values = ["hvm"]
+  }
 }
-
-}
-
 data "aws_ssm_parameter" "bastion_sg_id" {
-  name  = "/${var.project}/${var.environment}/bastion_sg_id"
+  name = "/${var.project}/${var.environment}/bastion_sg_id"
 }
 
 data "aws_ssm_parameter" "public_subnet_ids" {
