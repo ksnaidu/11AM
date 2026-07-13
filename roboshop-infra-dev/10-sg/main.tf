17c3f2c07444a8b3ac5db002ce8a7604a0427d24
ksnaidu <ksnaidudba@gmail.com>
2026-07-13 08:25:59 +0530

    backend-alb

--- roboshop-infra-dev/10-sg/main.tf
+++ roboshop-infra-dev/10-sg/main.tf
@@ -8,3 +8,45 @@
     vpc_id = local.vpc_id  ##vpc-id stored in ssm parameter after create.
 }
 
+
+module "bastion" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/ksnaidu/11AM.git//terraform-aws-securitygroup?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name = var.bastion_sg_name
+    sg_description = var.bastion_sg_description
+    vpc_id = local.vpc_id
+}
+
+
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion.sg_id
+}
+
+module "backend_alb" {
+  source = "git::https://github.com/ksnaidu/11AM.git//terraform-aws-securitygroup?ref=main"
+  project = var.project
+  environment = var.environment
+
+  sg_name = "backend-alb"
+  sg_description = "for backend-alb"
+  vpc_id = local.vpc_id ##vpc-id stored in ssm parameter after create.
+}
+
+
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.backend_alb.sg_id
+}
+
